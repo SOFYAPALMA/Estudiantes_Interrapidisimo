5ed9009fb5b18b42eff5ccde004fcb367a657e76
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-05-12 21:14:15 -0500

    Creacion de Mapper: mapeo entre las capas model y Dto

---
+++ Business/Mapp/Mapper.cs
@@ -0,0 +1,114 @@
+﻿using Domain.Model;
+using Dtos;
+
+namespace Business.Mapp
+{
+    public static class Mapper
+    {
+        public static List<EstudiantesDto> GetMapper(List<EstudiantesModel> listaEstudiantes)
+        {
+            var result = new List<EstudiantesDto>();
+
+            foreach (var model in listaEstudiantes)
+            {
+                result.Add(GetMapper(model));
+            }
+            return result;
+        }
+        public static EstudiantesModel GetMapper(EstudiantesDto estudiante)
+        {
+            var result = new EstudiantesModel()
+            {
+                id = estudiante.id,
+                nombre = estudiante.nombre,
+                apellido = estudiante.apellido
+            };
+            return result;
+        }
+        internal static EstudiantesModel GetMapper(EstudiantesCrearDto estudianteCrear)
+        {
+            var result = new EstudiantesModel()
+            {
+                id = estudianteCrear.id,
+                nombre = estudianteCrear.nombre,
+                apellido = estudianteCrear.apellido,
+                correo = estudianteCrear.correo,
+                password = estudianteCrear.password
+            };
+            return result;
+        }
+        internal static EstudiantesDto GetMapper(EstudiantesModel estudianteModel)
+        {
+            var result = new EstudiantesDto()
+            {
+                id = estudianteModel.id,
+                nombre = estudianteModel.nombre,
+                apellido = estudianteModel.apellido
+            };
+            return result;
+        }
+
+
+        internal static List<MateriaDto> GetMapper(List<MateriaModel> listaMateria)
+        {
+            var result = new List<MateriaDto>();
+
+            foreach (var materia in listaMateria)
+            {
+                result.Add(GetMapper(materia));
+            }
+            return result;
+        }
+        internal static MateriaModel GetMapper(MateriaDto materia)
+        {
+            var result = new MateriaModel()
+            {
+                id = materia.id,
+                nombreMateria = materia.nombreMateria
+            };
+            return result;
+        }
+        internal static MateriaDto GetMapper(MateriaModel materia)
+        {
+            var result = new MateriaDto()
+            {
+                id = materia.id,
+                nombreMateria = materia.nombreMateria
+            };                
+            return result;
+        }
+
+
+        internal static List<ProfesorDto> GetMapper(List<ProfesorModel> listaProfesor)
+        {
+            var result = new List<ProfesorDto>();
+
+            foreach (var profesor in listaProfesor)
+            {
+                result.Add(GetMapper(profesor));
+            }
+            return result;
+        }
+        internal static ProfesorModel GetMapper(ProfesorDto profesor)
+        {
+            var result = new ProfesorModel()
+            {
+                id = profesor.id,
+                nombre = profesor.nombre,
+                apellido = profesor.apellido
+            };
+            return result;
+        }
+        internal static ProfesorDto GetMapper(ProfesorModel profesor)
+        {
+            var result = new ProfesorDto()
+            {
+                id = profesor.id,
+                nombre = profesor.nombre,
+                apellido = profesor.apellido
+            };
+            return result;
+        }
+
+    }
+}
--- Dtos/EstudiantesDto.cs
+++ Dtos/EstudiantesDto.cs
@@ -5,8 +5,6 @@
         public int id { get; set; }
         public string nombre { get; set; }
         public string apellido { get; set; }
-        //public string correo { get; set; }
-        //public string password { get; set; }
+     
     }
 }
-s
